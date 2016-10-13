03901c0c07c73142119ca3654b36d6dda2c6c076
panjyp <panjyp@yahoo.com>
2016-10-13 15:04:59 -0500

    Initial Commit

---
+++ WatchCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchCollector
+//
+//  Created by Jeff Pan on 10/13/16.
+//  Copyright © 2016 Jeff Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
